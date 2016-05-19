1a6c660ea7a8ab70e7a4c81c3ec8b054f4cbacb6
Ken Dreyer <kdreyer@redhat.com>
2016-05-18 19:55:33 -0600

    setup.py: run flake8 checks during "setup.py test"

--- setup.py
+++ setup.py
@@ -61,7 +61,7 @@ class PyTest(TestCommand):
 
     def run_tests(self):
         import pytest
-        errno = pytest.main('rhcephcompose/tests ' + self.pytest_args)
+        errno = pytest.main('rhcephcompose --flake8 ' + self.pytest_args)
         sys.exit(errno)
 
 setup(
@@ -82,6 +82,7 @@ setup(
     ],
     tests_require=[
         'pytest',
+        'pytest-flake8',
     ],
     cmdclass={'test': PyTest, 'release': ReleaseCommand},
 )
