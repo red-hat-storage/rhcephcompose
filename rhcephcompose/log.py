f2732f6e88371d9d81beb875c17072f4b1d3a7d1
Ken Dreyer <kdreyer@redhat.com>
2016-05-23 17:11:58 -0600

    log.py: silence requests' INFO logging
    Requests (or urllib3?) prints many messages like:
    
      INFO: Starting new HTTPS connection (1): chacra.example.com
    
    Silence these.

--- rhcephcompose/log.py
+++ rhcephcompose/log.py
@@ -2,3 +2,7 @@ import logging
 
 logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.INFO)
 log = logging.getLogger('rhcephcompose')
+
+# Silence requests' INFO-level logging.
+logging.getLogger('requests').setLevel(logging.WARNING)
+logging.getLogger('urllib3').setLevel(logging.WARNING)
