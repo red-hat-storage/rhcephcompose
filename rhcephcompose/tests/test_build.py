f775cdca1cb370fc1095b147caf3c72aaf7d1837
Ken Dreyer <kdreyer@redhat.com>
2017-01-20 13:42:09 -0700

    tests: validate .name and .version attrs
    Prior to this change, we were not testing that these properies were
    correctly set. Do so here.

--- rhcephcompose/tests/test_build.py
+++ rhcephcompose/tests/test_build.py
@@ -6,5 +6,7 @@ class TestBuild(object):
     def test_constructor(self):
         b = Build('mypackage_1.0-1')
         assert b.build_id == 'mypackage_1.0-1'
+        assert b.name == 'mypackage'
+        assert b.version == '1.0-1'
         assert b.binaries == []
         assert b.sources == []
