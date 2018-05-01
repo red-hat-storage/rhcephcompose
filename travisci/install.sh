3c13459b0d7fd0f04885572f1a4504f91fc4e6ce
Ken Dreyer <kdreyer@redhat.com>
2018-05-01 11:23:22 -0600

    travisci: update kobo versions
    eng-rhel-6 currently has kobo 0.6.0.
    
    Python 3 support is available as of kobo 0.7.0, so we don't need to
    install from Git any more.

--- travisci/install.sh
+++ travisci/install.sh
@@ -4,11 +4,8 @@ set -euv
 
 # Install old eng-rhel-6 libs for py26
 if [ ${TRAVIS_PYTHON_VERSION:0:3} == 2.6 ]; then
-  pip install pytest==2.3.5 py==1.4.15 requests==2.3.0
+  pip install pytest==2.3.5 py==1.4.15 requests==2.3.0 kobo==0.6.0
 fi
-
-# https://github.com/release-engineering/kobo/pull/48
-pip install git+git://github.com/frenzymadness/kobo.git@python3-dev
 
 # We can run the flake8 tests on py27 and above
 if [ ${TRAVIS_PYTHON_VERSION:0:3} != 2.6 ]; then
