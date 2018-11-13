0bdd099f4af66c4530f6c6700a3e7add81d7b72b
Ken Dreyer <kdreyer@redhat.com>
2018-11-13 11:58:09 -0700

    travisci: install koji
    Install the koji package from pypi on the py27+ environments.

--- travisci/install.sh
+++ travisci/install.sh
@@ -7,9 +7,11 @@ if [ ${TRAVIS_PYTHON_VERSION:0:3} == 2.6 ]; then
   pip install pytest==2.3.5 py==1.4.15 requests==2.3.0 kobo==0.6.0
 fi
 
-# We can run the flake8 tests on py27 and above
 if [ ${TRAVIS_PYTHON_VERSION:0:3} != 2.6 ]; then
+  # We can run the flake8 tests on py27 and above
   pip install pytest-flake8
+  # And the koji tests
+  pip install koji
 fi
 
 pip install pytest-cov python-coveralls
